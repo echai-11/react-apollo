7198c73b6e9bd822b6af88a79c7180280ee1e098
Elizabeth Chai <lizzie.chai@flymya.co>
2019-12-10 09:09:42 +0630

    init commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
+import {setContext} from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Here you create the httpLink that will connect your 
+//ApolloClient instance with the GraphQL API, 
+//your GraphQL server will be running on http://localhost:4000.
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+})
+//Authetication
+//This middleware will be invoked every time ApolloClient sends a request to the server. 
+//Apollo Links allow you to create middlewares that let 
+//you modify requests before they are sent to the server.
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem(AUTH_TOKEN)
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    }
+  })
+//Now you instantiate ApolloClient 
+//by passing in the httpLink and a new instance of an InMemoryCache.
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+})
+
+//Finally you render the root component of your React app. 
+//The App is wrapped with the higher-order component ApolloProvider 
+//that gets passed the client as a prop.
+ReactDOM.render(
+    <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+    </BrowserRouter>,
+    document.getElementById('root')
+  )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
